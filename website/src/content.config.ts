17dfa05b6bef56b9dfa305d76089b279feaaff7d
Diovani Coelli <59318991+Diovani-C@users.noreply.github.com>
2025-10-07 10:23:33 -0300

    Add base layout
    Build a base layout component
    Add the gallery page
    Load image with the Astro content collection and display with the
    <Image/> component

--- website/src/content.config.ts
+++ website/src/content.config.ts
@@ -7,13 +7,14 @@ import { file } from "astro/loaders";
 // 3. Define your collection(s)
 const images = defineCollection({
   loader: file("src/data/images-metadata.json"),
-  schema: z.object({
-    slug: z.string(),
-    src: z.string(),
-    alt: z.string(),
-    raw: z.string().optional(),
-    categories: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      slug: z.string(),
+      src: image(),
+      alt: z.string(),
+      raw: z.string().optional(),
+      categories: z.array(z.string()),
+    }),
 });
 
 // 4. Export a single `collections` object to register your collection(s)
