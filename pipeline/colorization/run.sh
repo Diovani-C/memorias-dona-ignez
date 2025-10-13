58f344e28610c26b37d8a9b1d5d302095458b80f
Diovani Coelli <59318991+Diovani-C@users.noreply.github.com>
2025-10-13 11:28:41 -0300

    Add the spandrel library back
    Add the docker with python restoration models using spandrel

--- pipeline/colorization/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --gpus all -it \
-  -v ./output:/app/output:z \
-  -v ./input:/app/input:z \
-  ddcolor-colorization \
-  python run.py --model_path pretrain/ddcolor_modelscope.pth --input ./input --output ./output
---
+++ pipeline/spandrel/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# sudo podman run --gpus all -it --entrypoint /bin/bash \
+# spandrel
+
+docker run --gpus all -it \
+  -v ./output:/app/output:z \
+  -v ./input:/app/input:z \
+  spandrel \
+  python run.py -i input -o output --task deblur --cpu
